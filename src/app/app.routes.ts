f8ad0b3f8c4f65d1d8e5270ad5edc246307ab24f
fredyhg <fredyultima2@gmail.com>
2024-05-04 23:08:35 -0300

    feature: create raffle area and change colors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {RaffleZoneComponent} from "./components/raffle-zone/raffle-zone.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'raffle', component: RaffleZoneComponent}
+];
---
+++ src/app/components/raffle-zone/raffle-zone.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raffle-zone',
+  standalone: true,
+  imports: [],
+  templateUrl: './raffle-zone.component.html',
+  styleUrl: './raffle-zone.component.scss'
+})
+export class RaffleZoneComponent {
+
+}
