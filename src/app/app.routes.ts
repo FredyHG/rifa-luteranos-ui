05c73bf4629e6db42e46899e1e343c501df677fc
fredyhg <fredyultima2@gmail.com>
2024-05-06 13:34:08 -0300

    style: improve styles of cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import {RaffleZoneComponent} from "./components/raffle-zone/raffle-zone.component";
 
 export const routes: Routes = [
-  {path: 'raffle', component: RaffleZoneComponent}
+  {path: '', component: RaffleZoneComponent}
 ];
