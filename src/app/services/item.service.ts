52fc7fb89e1ca4862b1f6dd3e39cf73dd9462705
fredyhg <fredyultima2@gmail.com>
2024-05-07 11:44:07 -0300

    feat: create cart logic and implements cart badge indicator

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import {Item} from "../models/Item";
 
 @Injectable({
@@ -10,10 +10,26 @@ export class ItemService {
 
   private itemUrl = '/assets/items.json';
 
+  itemsInCart = new BehaviorSubject<Item[]>([]);
+  itemInCart$ = this.itemsInCart.asObservable();
+
+  currentItems: Item[] = [];
+
+
   constructor(private http: HttpClient) {}
 
   getItems(): Observable<Item[]>{
     return this.http.get<Item[]>(this.itemUrl);
+  }
+
+  addItemInCar(item: Item){
+    this.currentItems.push(item);
+    this.itemsInCart.next(this.currentItems);
+  }
+
+  removeItemInCar(itemToRemove: Item){
+    this.currentItems = this.currentItems.filter(item => item.name !== itemToRemove.name);
+    this.itemsInCart.next(this.currentItems);
   }
 
 }
