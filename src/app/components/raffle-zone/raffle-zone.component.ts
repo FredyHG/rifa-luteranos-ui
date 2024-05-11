0ebca7add2c6d11595f37a7f96923e9f1c22a016
fredyhg <fredyultima2@gmail.com>
2024-05-10 22:28:35 -0300

    feature: create table for raffles and att project to main version

--- src/app/components/raffle-zone/raffle-zone.component.ts
+++ src/app/components/raffle-zone/raffle-zone.component.ts
@@ -1,34 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ItemService} from "../../services/item.service";
 import {Item} from "../../models/Item";
-import {resolve} from "@angular/compiler-cli";
 import {NgForOf} from "@angular/common";
+import {CarouselModule} from "primeng/carousel";
+import {SpeedDialModule} from "primeng/speeddial";
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-raffle-zone',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    CarouselModule,
+    SpeedDialModule
   ],
   templateUrl: './raffle-zone.component.html',
   styleUrl: './raffle-zone.component.scss'
 })
-export class RaffleZoneComponent {
+export class RaffleZoneComponent implements OnInit{
 
   items: Item[] = [];
+  itemsFloatButton: MenuItem[] = [];
 
   constructor(private itemService: ItemService) {
     this.getItems();
+  }
+
+  ngOnInit(): void {
+    this.itemsFloatButton = [
+      {
+        icon: 'pi pi-shopping-cart',
+        command: () => {
+          console.log('clicked')
+        }
+      },
+    ];
   }
 
   getItems(): void{
     this.itemService.getItems().subscribe({
       next: response => {
         this.items = response
-        console.log(this.items)
       },
 
     })
   }
+
+  addCart(itemToAdd: Item): void{
+
+    this.items = this.items.filter(item => item.name !== itemToAdd.name);
+    this.itemService.addItemInCar(itemToAdd);
+  }
+
+  removeItem(item: Item) {
+    this.itemService.removeItemInCar(item);
+  }
+
 
 }
